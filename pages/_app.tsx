baf7e57cb71e83b15fee1f50a41b8870b313f2fa
Dmitry Bardyshev <brd.dmitry@gmail.com>
2022-03-26 23:44:54 +0300

    Updated favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import "../styles/index.css";
 import { Container, Layout, UI, useDeviceTheme } from "@bd-dm/ui";
 import { Provider } from "react-redux";
 import { store } from "store";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => {
 	const theme = useDeviceTheme();
@@ -13,6 +14,10 @@ const App = ({ Component, pageProps }: AppProps) => {
 			<UI theme={theme}>
 				<Layout>
 					<Container>
+						<Head>
+							<title>Gallereee</title>
+							<link rel="icon" href="/favicon.svg" />
+						</Head>
 						<Component {...pageProps} />
 					</Container>
 				</Layout>
