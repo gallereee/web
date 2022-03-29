4d4376ae2cd63332bfa8eb34e7b8f045c2ee22c0
Dmitry Bardyshev <brd.dmitry@gmail.com>
2022-03-29 23:37:02 +0300

    Fixed icon for light theme

--- components/icons/LogoIcon/index.tsx
+++ components/icons/LogoIcon/index.tsx
@@ -1,4 +1,5 @@
 import { SVGProps, VFC } from "react";
+import styles from "./index.module.scss";
 
 const LogoIcon: VFC<SVGProps<SVGSVGElement>> = (props) => (
 	<svg
@@ -11,15 +12,15 @@ const LogoIcon: VFC<SVGProps<SVGSVGElement>> = (props) => (
 	>
 		<path
 			d="M141 70.5c0 38.936-31.564 70.5-70.5 70.5S0 109.436 0 70.5 31.564 0 70.5 0 141 31.564 141 70.5Z"
-			fill="#fff"
+			className={styles["accent-circle"]}
 		/>
 		<path
 			d="M130.425 70.5c0 33.096-26.829 59.925-59.925 59.925S10.575 103.596 10.575 70.5 37.405 10.575 70.5 10.575c33.096 0 59.925 26.83 59.925 59.925Z"
-			fill="#283845"
+			className={styles["accent-opposite-circle"]}
 		/>
 		<path
 			d="M121.612 70.5c0 28.229-22.883 51.113-51.112 51.113-28.229 0-51.112-22.884-51.112-51.113S42.27 19.388 70.5 19.388c28.229 0 51.112 22.883 51.112 51.112Z"
-			fill="#fff"
+			className={styles["accent-circle"]}
 		/>
 	</svg>
 );
